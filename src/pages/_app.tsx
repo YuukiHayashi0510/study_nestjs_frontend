76f51564ae62f256f5858d89125ebfbe66a6d820
YuukiHayashi0510 <yuu.tennis.10.21.6@gmail.com>
2022-12-06 13:53:13 +0900

    first commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,54 @@
+import '~/styles/globals.css'
+import type { AppProps } from 'next/app'
+import { MantineProvider } from '@mantine/core'
+import { useEffect } from 'react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import axios from 'axios'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      // クエリの取得ミスった時に自動で3回取得するやつ
+      retry: false,
+      // ブラウザにフォーカスしたときにフェッチ
+      refetchOnWindowFocus: false,
+    },
+  },
+})
+
+export function MyApp(props: AppProps) {
+  const { Component, pageProps } = props
+  // cookieを使うときにTrue
+  axios.defaults.withCredentials = true
+
+  useEffect(() => {
+    const getCsrfToken = async () => {
+      const { data } = await axios.get(
+        `${process.env.NEXT_PUBLIC_API_URL}/auth/csrf`
+      )
+      // apiにリクエスト送るときはずっとcookie設定,csrfを追加できる
+      axios.defaults.headers.common['csrf-token'] = data.csrfToken
+    }
+    getCsrfToken()
+  }, [])
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          /** Put your mantine theme override here */
+          colorScheme: 'dark',
+          fontFamily: 'Verdana, sans-serif',
+        }}
+      >
+        <Component {...pageProps} />
+      </MantineProvider>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  )
+}
+
+export default MyApp
